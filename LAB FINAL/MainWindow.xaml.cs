96071320d7a3110d8a66805266d34575034a4267
MuhammadFatiq <fatiqmuhammad683@gmail.com>
2024-01-01 12:28:05 +0500

    first commit

---
+++ LAB FINAL/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace LAB_FINAL
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string connectionString = "YourConnectionStringHere";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDataFromDatabase();
+        }
+
+        private void LoadDataFromDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT FirstName, LastName FROM YourTableName";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string firstName = reader["FirstName"].ToString();
+                            string lastName = reader["LastName"].ToString();
+
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
